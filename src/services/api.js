e28f8c47bfe760a0ebf811ba83c8ad1a7f1cda3d
pranav1401-kumar <91305074+pranav1401-kumar@users.noreply.github.com>
2025-03-01 11:19:28 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://globetrotterbackend-production.up.railway.app/';
+const API_URL = process.env.REACT_APP_API_URL || 'https://globetrotterbackend-production.up.railway.app';
 
 // Destination API calls
 export const getRandomDestination = async () => {
